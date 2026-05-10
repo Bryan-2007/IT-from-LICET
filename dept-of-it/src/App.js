69ae720b87059574747f72454c28481849bce872
FemilaCS <167897198+FemilaCS@users.noreply.github.com>
2026-05-10 16:02:07 +0530

    adding image to 'about us'

--- dept-of-it/src/App.js
+++ dept-of-it/src/App.js
@@ -199,15 +199,17 @@ const About = () => {
         <>
             <section className="about" id="about">
                 <h2>About Us</h2>
-                <figure className="about-image-placeholder">
-                    <img src="/images/it-group.jpg" alt="LICET Information Technology department group" />
-                </figure>
+
                 <p>
                     The Information Technology (IT) Department at Loyola-ICAM College of Engineering and Technology (LICET)
                     was founded in 2010. It was established alongside the college itself, which was founded through a joint
                     venture between the Loyola College Society and ICAM, France. The department was created to provide
                     technical education focusing on bridging the gap between academia and industry.
                 </p>
+
+                <figure className="about-image-placeholder">
+                    <img src="/images/it-group.jpg" alt="LICET Information Technology department group" />
+                </figure>
 
                 <div className="vision-mission">
                     <div>
